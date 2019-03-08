dd2403560e86d3568667a5b87d939a3ec3d6806a
Tatyana <tatyana@ascendify.com>
2019-03-08 09:46:30 -0800

    Added 'Forgot your password' flow

--- src/main/java/tests/web/AscendifyTest.java
+++ src/main/java/tests/web/AscendifyTest.java
@@ -14,15 +14,14 @@ public class AscendifyTest {
     public static void setUp() {
         Web.ascendify.openTalentCommunityWorks();
     }
-//
-//    @AfterClass
-//    public static void tearDown() {
-//        System.out.println("closing the Driver");
-//        Ascendify.getDriver().quit();
-//    }
+
+    @AfterClass
+    public static void tearDown() {
+        System.out.println("closing the Driver");
+        Ascendify.getDriver().quit();
+    }
 
 
-    @Ignore
     @Test
     public void t1_signIn() {
         System.out.println("executing signIn");
@@ -34,69 +33,9 @@ public class AscendifyTest {
     }
 
 
-    @Test
-    public void forgot_password() throws InterruptedException {
-        System.out.println("executing fyp");
-        Web.ascendify.forgotYourPassword
-                .clickSignin()
-                .confirmClickSignin()
-                .clickForgotPassword()
-                .confirmClickForgotPassword()
-                .inputEmail("qa.ascendify@gmail.com")
-                .clickRecoverPasword()
-                .confirmClickRecoverPassword();
-
-    }
-
 
     @Test
-    public void gmail() throws InterruptedException {
-        Web.ascendify.gmailForgotYourPasswordEmail
-                .goToGmail()
-                .confirmGoToGmail()
-                .insertEmail("qa.ascendify@gmail.com")
-                .clickNext()
-                .confirmClickNext()
-                .insertPasswordAndClickEnter("Testing12345")
-                .confirmSignin()
-                .waitForPasswordResetEmail()
-                .confirmInsidePasswordResetEmail()
-                .clickPasswordResetLinkWorks();
-
-        Web.ascendify.forgotYourPassword
-                .resetNewPasswordWorks("Testing123");
-                //.confirmNewPassword("Testing123")
-                //.clickChangePassword();
-        Web.ascendify.gmailForgotYourPasswordEmail
-                .confirmSignin()
-                //.waitForPasswordResetEmail()
-                //.clickPasswordResetLink()
-                .clickMoreSettings()
-                .clickMoreSettingsDelete();
-    }
-
-    @Ignore
-    @Test
-    public void testTrashIcon() throws InterruptedException {
-        Web.ascendify.gmailForgotYourPasswordEmail
-                .goToGmail()
-                .confirmGoToGmail()
-                .insertEmail("qa.ascendify@gmail.com")
-                .clickNext()
-                .confirmClickNext()
-                .insertPasswordAndClickEnter("Testing12345")
-                .confirmSignin()
-                .waitForPasswordResetEmail()
-                //.clickPasswordResetLink()
-                //.clickTrashIcon()
-                .clickMoreSettings()
-                .clickMoreSettingsDelete()
-                .confirmNoEmails();
-    }
-
-    @Ignore
-    @Test
-    public void t3_signOut() {
+    public void t2_signOut() {
         System.out.println("executing signOut");
         Web.ascendify.signOut
                 .clickProfileDropdown()
